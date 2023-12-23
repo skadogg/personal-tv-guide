b658f1e6c63276b3b3dd87fce5edb03b48b86c6f
skadogg <wesanderson@gmail.com>
2023-12-23 13:05:24 -0600

    Changes shield function to act on only one element

--- function_generate_shield.py
+++ function_generate_shield.py
@@ -14,24 +14,18 @@ def generate_shield(list):
     # --nord15: #b48ead;
     color_other = 'b48ead'
     
-    i = 0
-    for i in range(len(list)):
-        # ('Making It', 'S3 E1', '+7', 'One In a Million', 'Reality TV, Comedy', '44min', 'TV-PG')
-        show = list[i][0].replace('-','--').replace('?','')
-        
-        rating = list[i][6] if list[i][6] else ' '
-        if rating in ['TV-Y7','TV-G','TV-PG']:
-            rating_color = color_green
-        elif rating in ['TV-14','PG-13']:
-            rating_color = color_blue
-        elif rating in ['TV-MA','R','NC-17','X']:
-            rating_color = color_red
-        else:
-            rating_color = color_other
-        rating = rating.replace('-','--')
-        
-        # ep = list[i][1]
-        # ep_title = list[i][3]
-            
-        # print('<img src="https://img.shields.io/badge/' + show + ' - ' + rating + '-' + rating_color + '"><br>' + ep + ' ' + ep_title + '<br>')
-        print('<img src="https://img.shields.io/badge/' + show + ' - ' + rating + '-' + rating_color + '?labelColor=' + color_base + '">')
+    # ('Making It', 'S3 E1', '+7', 'One In a Million', 'Reality TV, Comedy', '44min', 'TV-PG')
+    show = list[0].replace('-','--').replace('?','')
+    
+    rating = list[6] if list[6] else ' '
+    if rating in ['TV-Y7','TV-G','TV-PG']:
+        rating_color = color_green
+    elif rating in ['TV-14','PG-13']:
+        rating_color = color_blue
+    elif rating in ['TV-MA','R','NC-17','X']:
+        rating_color = color_red
+    else:
+        rating_color = color_other
+    rating = rating.replace('-','--')
+    
+    return '<img src="https://img.shields.io/badge/' + show + ' - ' + rating + '-' + rating_color + '?labelColor=' + color_base + '">'
