61bf35b56624c88535fe0cd9d65e46ce4c9cd134
skadogg <wesanderson@gmail.com>
2023-12-30 14:00:12 -0600

    Fixes #22 - Python progress bars while scraping?

--- scrape_tv.py
+++ scrape_tv.py
@@ -10,6 +10,7 @@ import time
 # import json
 import modules.ld_json
 import modules.auto_sign_in
+from alive_progress import alive_bar
 
 
 # Open main window
@@ -28,15 +29,22 @@ modules.auto_sign_in.sign_in(driver)
 
 # Scroll to the end of the page
 items_in_list = 60
+if dev_mode:
+    items_in_list = 5
 pages = (items_in_list // 20) + 1
 i = 0
-for i in range(pages):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    time.sleep(.5)
+with alive_bar(pages, spinner='waves', bar='squares') as bar:
+    for i in range(pages):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        time.sleep(.5)
+        bar()
 
 
 # Get name, episode number/title, left in season, main show link from main watchlist
 show_cards = driver.find_elements(By.XPATH, '//div[@class="title-card-basic title-card-show-episode"]')
+
+if dev_mode:
+    show_cards = show_cards[0:5]
 
 i = 0
 show_card_all_links = []
@@ -57,8 +65,6 @@ show_name = []
 episode_number = []
 episode_left_in_season = []
 episode_title = []
-# show_year = []
-# media_type = []
 for i in range(len(show_card_full_text)):
     this_show_elements = show_card_full_text[i].split(sep='\n')
     show_name.append(this_show_elements[1])
@@ -79,49 +85,53 @@ show_age_rating = []
 year = []
 media_type = []
 synopsis = []
-for j in range(len(show_main_link)):
-    full_url = 'https://www.justwatch.com' + show_main_link[j]
+with alive_bar(len(show_main_link), spinner='waves', bar='squares') as bar:
+    for j in range(len(show_main_link)):
+        full_url = 'https://www.justwatch.com' + show_main_link[j]
 
-    # Get year, media type, age rating, and synopsis quickly from ld-json data
-    show_ld_json_data = modules.ld_json.get_ld_json(full_url)
+        # Get year, media type, age rating, and synopsis quickly from ld-json data
+        show_ld_json_data = modules.ld_json.get_ld_json(full_url)
 
 
-    # Visit each page to get genres and runtimes
-    # from https://www.browserstack.com/guide/selenium-wait-for-page-to-load
-    driver.get(full_url)
-    try:
-        elem = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.XPATH, '//div[@class="title-info title-info"]'))
-        )
-    finally:
-        time.sleep(.5)
+        # Visit each page to get genres and runtimes
+        # from https://www.browserstack.com/guide/selenium-wait-for-page-to-load
+        driver.get(full_url)
+        try:
+            elem = WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located((By.XPATH, '//div[@class="title-info title-info"]'))
+            )
+        finally:
+            time.sleep(.5)
 
-    title_info = driver.find_element(By.XPATH, '//div[@class="title-info title-info"]')
-    detail_infos = title_info.find_elements(By.XPATH,'//div[@class="detail-infos"]')
+        title_info = driver.find_element(By.XPATH, '//div[@class="title-info title-info"]')
+        detail_infos = title_info.find_elements(By.XPATH,'//div[@class="detail-infos"]')
 
-    # Loop through each section on the page to get headings and text
-    k = 0
-    title_info_heading = []
-    title_info_value = []
-    shows_dict = []
-    for k in range(len(detail_infos)):
-        text = detail_infos[k].text
-        if len(text) > 0:
-            text_split = text.split(sep='\n')
-            split_head = text_split[0]
-            split_value = text_split[1]
-            title_info_heading.append(split_head)
-            title_info_value.append(split_value)
+        # Loop through each section on the page to get headings and text
+        k = 0
+        title_info_heading = []
+        title_info_value = []
+        shows_dict = []
+        for k in range(len(detail_infos)):
+            text = detail_infos[k].text
+            if len(text) > 0:
+                text_split = text.split(sep='\n')
+                split_head = text_split[0]
+                split_value = text_split[1]
+                title_info_heading.append(split_head)
+                title_info_value.append(split_value)
 
-    shows_dict = dict(zip(title_info_heading,title_info_value))
-    show_genres.append(shows_dict.get('GENRES'))
-    show_runtime.append(shows_dict.get('RUNTIME'))
-    # show_age_rating.append(shows_dict.get('AGE RATING'))
+        shows_dict = dict(zip(title_info_heading,title_info_value))
+        show_genres.append(shows_dict.get('GENRES'))
+        show_runtime.append(shows_dict.get('RUNTIME'))
+        # show_age_rating.append(shows_dict.get('AGE RATING'))
 
-    year.append(show_ld_json_data['dateCreated'].split('-')[0])
-    media_type.append(show_ld_json_data['@type'])
-    show_age_rating.append(show_ld_json_data['contentRating'])
-    synopsis.append(show_ld_json_data['description'])
+        year.append(show_ld_json_data['dateCreated'].split('-')[0])
+        media_type.append(show_ld_json_data['@type'])
+        show_age_rating.append(show_ld_json_data['contentRating'])
+        synopsis.append(show_ld_json_data['description'])
+        
+        bar.text = show_main_link[j]
+        bar()
 
 
 # driver.close()
