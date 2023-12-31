f8c498ba80b1ed376965c4949e874a5d4dfb6ee8
skadogg <wesanderson@gmail.com>
2023-12-30 23:58:57 -0600

    💩 Finds a way to evaluate dev_mode bool

---
+++ bool_junk.py
@@ -0,0 +1,18 @@
+
+# import dotenv
+import os
+
+# dotenv.load_dotenv()
+from dotenv import 
+
+dev_mode = os.environ.get('DEV_MODE').lower() == 'true'
+
+dotenv.get_key(
+    
+os.environ.get('DEV_MODE','False')
+
+bool()
+
+dotenv.bool(os.environ.get('DEV_MODE'))
+
+os.environ
