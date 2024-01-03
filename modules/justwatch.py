06d0110d8bdbcfe28c2f672310a869a9ad313b91
skadogg <wesanderson@gmail.com>
2024-01-02 22:39:13 -0600

    🚧 Starts working on MOTW

--- modules/justwatch.py
+++ modules/justwatch.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import random
 
 
 def get_titles_count(driver):
@@ -7,3 +8,18 @@ def get_titles_count(driver):
     titles_count_str = driver.find_element(By.XPATH, '//div[@class="titles-count"]').text
     titles_count = int(titles_count_str.split(' titles')[0])
     return titles_count
+
+
+def get_title_cover(title): # TODO:
+    # Takes a movie list and looks up the URL for the cover iamge
+    url = ''
+    return url
+
+
+def movie_of_the_week(list):
+    # Takes a random movie from the list and returns the chosen movie and the URL for the cover iamge
+    # Example: movie_of_the_week, img_url = movie_of_the_week(list)
+    random.shuffle(list)
+    show = list[0]
+    cover_url = get_title_cover(show)
+    return show, cover_url
