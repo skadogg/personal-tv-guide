dbd5023365df36bfb556f5a23a1b3143396fbb7a
skadogg <wesanderson@gmail.com>
2024-01-13 16:05:39 -0600

    Revert "🥅 Escapes the loop on error 🤞"
    This reverts commit 4d23d38ef64264741e966f82bf30cb8c4df367e0.

--- modules/justwatch.py
+++ modules/justwatch.py
@@ -194,7 +194,6 @@ def scrape_justwatch(media):
                     )
                 finally:
                     time.sleep(.5)
-                    continue
 
                 title_info = driver.find_element(By.XPATH, '//div[@class="title-info title-info"]')
                 detail_infos = title_info.find_elements(By.XPATH,'//div[@class="detail-infos"]')
